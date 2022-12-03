3059ba0e9ba54db2667e2ab77cf67f018fa2c6b1
Lovinder Pnag <lspnag@gmail.com>
2022-12-03 11:31:26 -0500

    added code but need new lines at end of input

--- day1/ans.rb
+++ day1/ans.rb
@@ -12,10 +12,40 @@ end
 
 # add calories
 
-cal_add = []
+
+# cal_add = []
+# File.readlines('test.txt').each do |line|
+#   num = line.to_i
+#   cal_add << num
+# end
+
+# Pseudocode 
+# each new line add to each other, except when there is a newline then add previous amount to array and begin adding new amount
+
+elf_total = []
+cal = []
+
 File.readlines('test.txt').each do |line|
   num = line.to_i
-  cal_add << num
+  if num != 0
+    cal << num
+  else
+    sum = 0
+    cal.each do |i|
+      sum += i
+    end
+    elf_total << sum
+    cal = []
+  end
+  
 end
 
-p cal_add
+
+# p cal
+p elf_total
+
+
+# p [1,2,3,4]
+
+
+
