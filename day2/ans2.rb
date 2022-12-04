74b3cb8d4a19c5678caad6284c894fef515f66ad
Lovinder Pnag <lspnag@gmail.com>
2022-12-04 08:39:16 -0500

    created hashes for game

---
+++ day2/ans2.rb
@@ -0,0 +1,19 @@
+# A for Rock, B for Paper, and C for Scissors.
+# X for Rock, Y for Paper, and Z for Scissors.
+
+# what you selected (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
+
+# A Y
+# B X
+# C Z
+
+
+# 15 (8 + 1 + 6)
+
+ans_A = {X: 3, Y: 6, Z: 0}
+
+ans_B = {X: 0, Y: 3, Z: 6}
+
+ans_C = {X: 6, Y: 0, Z: 3}
+
+
