be9946ad7012c1c92758112f7f24949cc22790dc
Lovinder Pnag <lspnag@gmail.com>
2022-12-07 05:37:34 -0500

    can find item in both compartments

--- day3/ans3.rb
+++ day3/ans3.rb
@@ -1,5 +1,35 @@
-
+# Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
 
 a = "vJrwpWtwJgWrhcsFMMfFFhFp"
+
+# b = a.split("")
+
+# c = b.count
+
+# d = c/2
+
+# using ampersand operator
+first, second = a.chars.each_slice(a.length/2).map(&:join)
+
+a = first.chars
+b = second.chars
+
+z = ""
+a.each do |x|
+  b.each do |y|
+    if x == y
+      z = y
+      break
+    end
+  end
+  if z != ""
+    break
+  end
+  puts x
+end
+
+
+
+
 
 
