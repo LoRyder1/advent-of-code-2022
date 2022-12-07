92c41a6839cc6ba68a490e33415dfd05f1273be2
Lovinder Pnag <lspnag@gmail.com>
2022-12-07 06:08:15 -0500

    solved part 1 day3

--- day3/ans3.rb
+++ day3/ans3.rb
@@ -1,7 +1,8 @@
 # Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
 
-a = "vJrwpWtwJgWrhcsFMMfFFhFp"
+a = "vJrwpWtwJgWrhcsFMMfFFhFpx"
 
+# puts a.chars.count
 # b = a.split("")
 
 # c = b.count
@@ -11,24 +12,62 @@ a = "vJrwpWtwJgWrhcsFMMfFFhFp"
 # using ampersand operator
 first, second = a.chars.each_slice(a.length/2).map(&:join)
 
+
+
 a = first.chars
 b = second.chars
 
-z = ""
-a.each do |x|
-  b.each do |y|
-    if x == y
-      z = y
+# p a 
+
+p1 = ('a'..'z').to_a
+p2 = ('A'..'Z').to_a
+
+p3 = p1 + p2
+
+# p p3
+
+
+
+# bingo = p3.index z
+
+# p bingo + 1
+
+
+sum = 0
+File.readlines("input.txt").each do |line|
+
+  # puts line.chars.count
+  first, second = line.chars.each_slice(line.length/2).map(&:join)
+
+  a = first.chars
+  b = second.chars
+  z = ""
+  a.each do |x|
+    b.each do |y|
+      if x == y
+        z = y
+        break
+      end
+    end
+    if z != ""
       break
     end
+    # puts x
   end
-  if z != ""
-    break
-  end
-  puts x
+
+  bingo = p3.index z
+  bingo +=1
+
+  sum += bingo
+
 end
 
+p sum
 
+
+
+# p bingo + 1
+ 
 
 
 
