e8b9879a7c553df8ef021e935d1cec937c7852c7
Lovinder Pnag <lspnag@gmail.com>
2022-12-06 19:38:07 -0500

    more work

--- day2/ans2.rb
+++ day2/ans2.rb
@@ -24,42 +24,110 @@ selection = {"X" => 1, "Y" => 2, "Z" => 3}
 
 # line = "A Y"
 
-# Round1 = A X
-# Round2 = B X
-# Round3 = C X
+# Round1 = A X = Y draw
+# Round2 = B X = X lose
+# Round3 = C X = Z win
 
 # Count rounds - draw, lose, win
 
 
 total_points = 0
+count = 0
+
 
 File.readlines('input2.txt').each do |line|
+  count += 1
 
-  if line[0] == "A"
-    player_2_play = "X"
-    points = ans_A[player_2_play]
-    play2_sel = selection[player_2_play]
 
-    total_points += points + play2_sel
-    # total_points += play2_sel
-  elsif line[0] == "B"
-    player_2_play = "X"
-    points = ans_B[player_2_play]
-    play2_sel = selection[player_2_play]
+  if count%3 == 1
+    # round 1 draw
+    # puts count
+    # puts count%3
+    # puts 'whoa'
 
-    total_points += points + play2_sel
-    # total_points += play2_sel
-  else
-    player_2_play = "X"
-    points = ans_C[player_2_play]
-    play2_sel = selection[player_2_play]
 
-    total_points += points + play2_sel
-    # total_points += play2_sel
+    total_points += 3
+    if line[0] == 'A'
+      total_points += 1
+    elsif line[0] == 'B'
+      total_points += 2
+    else
+      total_points += 3
+    end
+
+
+  elsif count%2 == 2
+    # round 2 lose
+    # puts count
+    # puts count%3
+    # puts 'find'
+
+    total_points += 0
+    if line[0] == 'A'
+      total_points += 3
+    elsif line[0] == 'B'
+      total_points += 1
+    else
+      total_points += 2
+    end
+  
+  else count%3 == 0
+    # round 3 win
+    # puts count
+    # puts count%3
+    # puts 'hey'
+
+    total_points += 6
+
+    if line[0] == 'A'
+      total_points += 2
+    elsif line[0] == 'B'
+      total_points += 3
+    else 
+      total_points += 1
+    end
   end
+
+
+
 end
 
+# 12566 = too low
+# 17784 too high
 puts total_points
+
+
+
+# File.readlines('input2.txt').each do |line|
+
+# count +=1
+
+#   if line[0] == "A"
+#     player_2_play = "X"
+
+#     points = ans_A[player_2_play]
+#     play2_sel = selection[player_2_play]
+
+#     total_points += points + play2_sel
+#     # total_points += play2_sel
+#   elsif line[0] == "B"
+#     player_2_play = "X"
+#     points = ans_B[player_2_play]
+#     play2_sel = selection[player_2_play]
+
+#     total_points += points + play2_sel
+#     # total_points += play2_sel
+#   else
+#     player_2_play = "X"
+#     points = ans_C[player_2_play]
+#     play2_sel = selection[player_2_play]
+
+#     total_points += points + play2_sel
+#     # total_points += play2_sel
+#   end
+# end
+
+# puts total_points
 
 #9007 wrong
 # 11666
