127fb34f4c60294ff0d1a535f4020230a911a8dd
Lovinder Pnag <lspnag@gmail.com>
2022-12-13 05:08:52 -0500

    made some progress on 3 rucksacks comparison

--- day3/ans3.rb
+++ day3/ans3.rb
@@ -33,41 +33,89 @@ p3 = p1 + p2
 # p bingo + 1
 
 
-sum = 0
-File.readlines("input.txt").each do |line|
+# sum = 0
+# File.readlines("input.txt").each do |line|
 
-  # puts line.chars.count
-  first, second = line.chars.each_slice(line.length/2).map(&:join)
+#   # puts line.chars.count
+#   first, second = line.chars.each_slice(line.length/2).map(&:join)
 
-  a = first.chars
-  b = second.chars
-  z = ""
-  a.each do |x|
-    b.each do |y|
-      if x == y
-        z = y
-        break
-      end
-    end
-    if z != ""
-      break
-    end
-    # puts x
-  end
+#   a = first.chars
+#   b = second.chars
+#   z = ""
+#   a.each do |x|
+#     b.each do |y|
+#       if x == y
+#         z = y
+#         break
+#       end
+#     end
+#     if z != ""
+#       break
+#     end
+#   end
+#   bingo = p3.index z
+#   bingo +=1
 
-  bingo = p3.index z
-  bingo +=1
+#   sum += bingo
 
-  sum += bingo
+# end
 
-end
-
-p sum
-
+# p sum
 
 
 # p bingo + 1
  
+
+# Part 2
+
+sum = 0
+ruck_items = []
+ruck_num = 0
+File.readlines('abc').each do |line|
+  ruck_num +=1  
+  ruck_items << line
+
+  a = ""
+  b = ""
+  c = ""
+  if ruck_num %3 == 0
+    print ruck_items
+    ruck_items.each_with_index do |x, i|
+
+      if i == 0
+        a = x.chars
+      elsif i == 1
+        b = x.chars
+      else
+        c = x.chars
+      end  
+    end
+    a.each do |x|
+      b.each do |y|
+        c.each do |z|
+          if x == y && y == z
+            p z
+            break
+          end
+        end
+      end
+    end
+
+
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
